5b792cb813b7f7f7f0cfac6d26bb3aa9d1b7065b
tomevans <tom.evans@astro.ox.ac.uk>
2014-07-03 21:17:29 +0100

    Added data and script

---
+++ script.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+wavsols = [ 'wavsol0.txt', 'wavsol1.txt', 'wavsol2.txt' ]
+wavcors = [ 'wavcor0.txt', 'wavcor1.txt', 'wavcor2.txt' ]
+spectra = [ 'spectra0.txt', 'spectra1.txt', 'spectra2.txt' ]
+
+bandpass = [ 450, 475 ]
+fluxes = []
+for k in range( 3 ):
+    wavsol = np.loadtxt( wavsols[k] )
+    spectrum = np.loadtxt( spectra[k] )
+    ixs_bandpass = ( wavsol[0,:]>bandpass[0] )*( wavsol[0,:]<bandpass[1] )
+    fluxes += [ np.sum( spectrum[:,ixs_bandpass], axis=1 ) ]
+    
+plt.figure()
+plt.plot( fluxes[0]/( fluxes[1]+fluxes[2] ), '.k' )
+
